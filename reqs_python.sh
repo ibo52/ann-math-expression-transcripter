ec4ded5d98da2326014470249b0e8c4ff8ca393e
Halil Ibrahim Mut <halilibrahimmut@hotmail.com>
2023-01-14 17:47:27 +0300

    Add files via upload

---
+++ reqs_python.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "---------------------------------------------------------------"
+echo "python libraries installation for ANN_MATH_TRANSCRIPTER"
+echo "---------------------------------------------------------------"
+echo "automatic installation of library requirements(for ubuntu 20)" 
+echo "keras imutils numpy opencv-python matplotlib tensorflow will be installed!!"
+echo "It will take some time related to internet speed. Please wait patiently."
+echo "halilibrahimmut"
+echo "---------------------------------------------------------------"
+pip3 install keras imutils numpy opencv-python matplotlib tensorflow
+echo "Done."
